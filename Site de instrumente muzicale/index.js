83eacdf6fb773b97e1a35009d983fa924576d325
alexbuturuga <53307277+alexbuturuga@users.noreply.github.com>
2022-12-05 21:56:46 +0200

    Com

--- Site de instrumente muzicale/index.js
+++ Site de instrumente muzicale/index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 
 app = express();
+app.set("view engine", "ejs");
 
-app.get("/index.html", function(req, res){
-    console.log("ceva");
-    res.send("Nu vreai sa-ti dau index!!!");
+app.use("/resurse", express.static(__dirname+"/resurse"));
+
+
+app.get("/*", function(req, res){
+    console.log(req.url);
+    // res.sendFile(__dirname+ "/index.html");
+    if(req.url == "/home")
+        req.url = "/index";
+    res.render("pagini" + req.url, function(err,rezRandare)
+    {
+        console.log("Eroare", err);
+        console.log("Rezulatat randare", rezRandare);
+
+        if(err)
+        {
+            if(err.message.includes("Failed to lookup view"))
+            res.render("pagini/404")
+            else
+            if(err.message.includes("Forbidden"))
+            res.render("pagini/403")
+            else
+            res.render("pagini/503")
+            
+        }else
+        {
+            res.send(rezRandare);
+        }
+        
+    });
     
 })
 
