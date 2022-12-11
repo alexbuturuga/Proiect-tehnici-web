d4d77e2bef0feced9c7aaa1df0d3cdb03e3ba5e9
alexbuturuga <53307277+alexbuturuga@users.noreply.github.com>
2022-12-11 23:48:23 +0200

    com 1
    ss

--- Site de instrumente muzicale/index.js
+++ Site de instrumente muzicale/index.js
@@ -1,10 +1,58 @@
+const { text } = require("express");
 const express = require("express");
+const fs=require("fs");
 
 app = express();
 app.set("view engine", "ejs");
 
 app.use("/resurse", express.static(__dirname+"/resurse"));
 
+obGlobal={
+    erori:null,
+    imagini:null
+}
+
+function createImages(){
+    var continutFisier=fs.readFileSync(__dirname+"/resurse/json/galerie.json").toString("utf8");
+    //console.log(continutFisier);
+    var obiect=JSON.parse(continutFisier);
+    obGlobal.imagini = obiect.imagini;
+    obGlobal.imagini.forEach(function (elem){
+        elem.fisier = obiect.cale_galerie+ "/" + elem.fisier;
+    });
+   // console.log(obGlobal.imagini);
+}
+createImages();
+
+
+function createErrors(){
+    var continutFisier=fs.readFileSync(__dirname+"/resurse/json/erori.json").toString("utf8");
+    //console.log(continutFisier);
+    obGlobal.erori=JSON.parse(continutFisier);
+    //console.log(obErori.erori);
+}
+createErrors();
+
+function renderError(res, identificator, titlu, text, imagine){
+    var eroare = obGlobal.erori.info_erori.find(function(elem){
+        return elem.identificator==identificator;
+    })
+    titlu= titlu || (eroare && eroare.titlu) || obGlobal.erori.eroare_default.titlu;
+    text= text || (eroare && eroare.text) || obGlobal.erori.eroare_default.text;
+    imagine= imagine || (eroare && obGlobal.erori.cale_baza+"/"+eroare.imagine) || obGlobal.erori.cale_baza+"/"+obGlobal.erori.eroare_default.imagine;
+    if(eroare && eroare.status){
+        res.status(identificator).render("pagini/eroare", {titlu:titlu, text:text, imagine:imagine})
+    }
+    else{
+        res.render("pagini/eroare", {titlu:titlu, text:text, imagine:imagine});
+    }
+}
+
+
+app.get("/*.ejs", function(req, res)
+{
+    renderError(res,403);
+})
 
 app.get("/*", function(req, res){
     console.log(req.url);
@@ -19,12 +67,12 @@ app.get("/*", function(req, res){
         if(err)
         {
             if(err.message.includes("Failed to lookup view"))
-            res.render("pagini/404")
-            else
-            if(err.message.includes("Forbidden"))
-            res.render("pagini/403")
-            else
-            res.render("pagini/503")
+                {
+                    renderError(res,404,"titlu custom");
+                }else
+                {
+
+                }
             
         }else
         {
