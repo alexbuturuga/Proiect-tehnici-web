dfa31b5e1f4b4e61cdf423b2ece2796728be57ac
alexbuturuga <53307277+alexbuturuga@users.noreply.github.com>
2022-12-22 13:24:51 +0200

    asa

--- Site de instrumente muzicale/index.js
+++ Site de instrumente muzicale/index.js
@@ -2,7 +2,8 @@ const { text } = require("express");
 const express = require("express");
 const fs=require("fs");
 const sharp=require("sharp");
-
+const ejs = require("ejs");
+const sass = require("sass");
 app = express();
 app.set("view engine", "ejs");
 
@@ -60,6 +61,37 @@ function renderError(res, identificator, titlu, text, imagine){
         res.render("pagini/eroare", {titlu:titlu, text:text, imagine:imagine});
     }
 }
+
+
+app.get("*/galerie-animata.css",function(req, res){
+
+    var sirScss=fs.readFileSync(__dirname+"/resurse/scss/galerie_animata.scss").toString("utf8");
+    var culori=["navy","black","purple","grey"];
+    var indiceAleator=Math.floor(Math.random()*culori.length);
+    var culoareAleatoare=culori[indiceAleator]; 
+    rezScss=ejs.render(sirScss,{culoare:culoareAleatoare});
+    console.log(rezScss);
+    var caleScss=__dirname+"/temp/galerie_animata.scss"
+    fs.writeFileSync(caleScss,rezScss);
+    try {
+        rezCompilare=sass.compile(caleScss,{sourceMap:true});
+        
+        var caleCss=__dirname+"/temp/galerie_animata.css";
+        fs.writeFileSync(caleCss,rezCompilare.css);
+        res.setHeader("Content-Type","text/css");
+        res.sendFile(caleCss);
+    }
+    catch (err){
+        console.log(err);
+        res.send("Eroare");
+    }
+});
+
+app.get("*/galerie-animata.css.map",function(req, res){
+    res.sendFile(path.join(__dirname,"temp/galerie-animata.css.map"));
+});
+
+
 
 
 app.get("/*.ejs", function(req, res)
